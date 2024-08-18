5d10902dec6701a8160da94bb5f2cceeacda03c7
Lakshay Gola <lakshaygola@Lakshays-Laptop.local>
2024-08-18 19:08:39 +0530

    Resolve card and screen height issue

--- frontend/src/components/NewsCard.tsx
+++ frontend/src/components/NewsCard.tsx
@@ -12,13 +12,13 @@ interface Props {
 export default function NewsCard(props: Props){
 
     return (
-        <div className="news-card bg-slate-100 flex flex-col w-80 py-3 flex-grow-1 rounded mb-6 transition ease-in-out hover:scale-105">
+        <div className="news-card bg-slate-100 flex flex-col w-80 py-3 h-fit flex-grow-1 rounded mb-6 transition ease-in-out hover:scale-105">
             <Link to={props.url}>
-            <div className="h-52 w-full">
+            <div className="w-full">
                 <img src={props.imageUrl} alt="image"  className="object-fill min-h-675 min-w-1200"/>
             </div>
             <div className="news-heading font-serif text-left px-2 py-2 font-semibold">{props.title}</div>
-            <div className="news-description font-serif text-left px-2 text-sm h-20 truncate">{props.description}</div>
+            <div className="news-description font-serif text-left px-2 text-sm truncate">{props.description}</div>
             <div className="news-author text-xs text-gray-700 p-2">{props.author}  |  {props.publishedAt}</div>
             </Link>
         </div>
